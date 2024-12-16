4da27f2d72285b26f9adef566828fd38ff9f873d
K1-mikaze <K1mikaze@proton.me>
2024-12-16 15:17:43 -0500

    more bindigins

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -17,6 +17,48 @@
       action = "<cmd>w<CR>";
       key = "<C-s>";
     }
+    # new actions
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').lsp_references()<CR>";
+      key = "<leader>lr";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').lsp_document_symbols()<CR>";
+      key = "<leader>lf";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').diagnostics()<CR>";
+      key = "<leader>le";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').lsp_definitions()<CR>";
+      key = "<leader>ld";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').lsp_implementations()<CR>";
+      key = "<leader>li";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').lsp_type_definitions()<CR>";
+      key = "<leader>lt";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').git_commits()<CR>";
+      key = "<leader>gc";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').git_branches()<CR>";
+      key = "<leader>gb";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').git_status()<CR>";
+      key = "<leader>gs";
+    }
+    { 
+      action =  "<cmd>:lua require('telescope.builtin').treesitter(require('telescope.themes').get_ivy({}))<CR>";
+      key = "<leader>tv";
+    }
+
 
   ];
 
